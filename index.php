bf16487d4a094554b940cdf9d21af25bceb24a9d
Osvaldo A. Neto <oalves.neto@outlook.com>
2024-06-03 20:27:06 -0300

    Testing getters

--- index.php
+++ index.php
@@ -5,16 +5,18 @@ require_once 'vendor/autoload.php';
 
 $filme = new Movie(
     null,
-    'Top Gun: Maverick',
+    'Top Gun - Maverick',
     2022,
     'Ação'
 );
 
 $filme->rate(10);
 $filme->rate(8.6);
-$filme->rate(7.4);
+$filme->rate(3.4);
 $filme->rate(9.2);
 $filme->rate(6.5);
 
-var_dump($filme);
-echo $filme->average();
+echo "Nome: {$filme->getName()}" . PHP_EOL;
+echo "Ano de Lançamento: {$filme->getRelease()}" . PHP_EOL;
+echo "Genêro: {$filme->getGenre()}" . PHP_EOL;
+echo "Recomendação: {$filme->average()}/10" . PHP_EOL;
