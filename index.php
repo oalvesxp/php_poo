5fb578a4347d5d0b56f5cbb345ec9702f78d8a28
Osvaldo A. Neto <oalves.neto@outlook.com>
2024-06-03 20:49:21 -0300

    Generating the new object from class Movie

--- index.php
+++ index.php
@@ -10,13 +10,31 @@ $filme = new Movie(
     'Ação'
 );
 
+$filme2 = new Movie(
+    null,
+    'Spider-man 2',
+    2004,
+    'Super-herói'
+);
+
 $filme->rate(10);
 $filme->rate(8.6);
 $filme->rate(3.4);
 $filme->rate(9.2);
-$filme->rate(6.5);
+
+$filme2->rate(10);
+$filme2->rate(9.6);
+$filme2->rate(7.4);
+$filme2->rate(9.2);
 
 echo "Nome: {$filme->getName()}" . PHP_EOL;
 echo "Ano de Lançamento: {$filme->getRelease()}" . PHP_EOL;
 echo "Genêro: {$filme->getGenre()}" . PHP_EOL;
 echo "Recomendação: {$filme->average()}/10" . PHP_EOL;
+
+echo PHP_EOL;
+
+echo "Nome: {$filme2->getName()}" . PHP_EOL;
+echo "Ano de Lançamento: {$filme2->getRelease()}" . PHP_EOL;
+echo "Genêro: {$filme2->getGenre()}" . PHP_EOL;
+echo "Recomendação: {$filme2->average()}/10" . PHP_EOL;
