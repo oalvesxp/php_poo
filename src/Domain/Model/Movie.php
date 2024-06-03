24adba91b3d7548d03ffe9077f5f47e967a13aef
OsvaldoNeto <osvaldo.neto@signus.ind.br>
2024-06-03 15:11:52 -0300

    Adding methods to save the Movie rated and calculate the average rates

--- src/Domain/Model/Movie.php
+++ src/Domain/Model/Movie.php
@@ -8,15 +8,31 @@ Class Movie {
     private string $name;
     private ?int $release;
     private string $genre;
-    private float $rate;
+    
+     /** Avaliações */
+    private array $rates = [];
 
     /** Construtor da classe filme */
-    public function __construct(?int $id, string $name, ?int $release, string $genre, float $rate)
+    public function __construct(?int $id, string $name, ?int $release, string $genre)
     {
         $this->id = $id;
         $this->name = $name;
         $this->release = $release;
         $this->genre = $genre;
-        $this->rate = $rate;
+    }
+
+    /** Recebe avaliações do Filme */
+    public function rate(float $value): void
+    {
+        $this->rates[] = $value;
+    }
+
+    /** Calcula as avaliações do Filme */
+    public function average(): float
+    {
+        $sumRates = array_sum($this->rates);
+        $countRates = count($this->rates);
+
+        return $sumRates / $countRates;
     }
 }
