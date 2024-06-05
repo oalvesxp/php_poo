892ed78b7b7ee8b724e8335d13a30eda35f8c3f3
Osvaldo A. Neto <oalves.neto@outlook.com>
2024-06-04 22:11:23 -0300

    Changing the movie class to inherit the Title class

--- src/Domain/Model/Movie.php
+++ src/Domain/Model/Movie.php
@@ -2,41 +2,21 @@
 
 namespace Locadora\Poo\Domain\Model;
 
-Class Movie 
+Class Movie extends Tittle
 {
-     /** Avaliações */
-    private array $rates;
-
-    /** Construtor da classe filme */
+    /** Construtor da classe Filme */
     public function __construct(
-        public readonly ?int $id,
-        public readonly string $name,
-        public readonly ?int $release,
-        public readonly Genre $genre,
-        public readonly ?int $timeMin
-        )
-    {
-        $this->rates = [];
-    }
-
-    /** Getters */
-    public function getGenre(): string
-    {
-        return $this->genre->value;
-    }
-
-    /** Recebe avaliações do Filme */
-    public function rate(float $grade): void
-    {
-        $this->rates[] = $grade;
-    }
-
-    /** Calcula as avaliações do Filme */
-    public function average(): float
-    {
-        $sumRates = array_sum($this->rates);
-        $countRates = count($this->rates);
-
-        return $sumRates / $countRates;
+        ?int $id,
+        string $name,
+        ?int $release,
+        Genre $genre,
+        public readonly ?int $timeMin,
+    ) {
+        parent::__construct(
+            $id,
+            $name,
+            $release,
+            $genre
+        );
     }
 }
